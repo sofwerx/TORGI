f511c7f5b35424a54970c80c4e4d3b82dc1cd3da
Jacob Fletcher <jacobfletcher0293@floridapoly.edu>
2018-10-02 12:53:57 -0400

    Merge branch 'master' into gad-dev

--- analysis/Python/main.py
+++ analysis/Python/main.py
@@ -1,21 +1,36 @@
-
-
 import sqlite3
 import csv
+import os
+import pandas as pd
 
 con = sqlite3.connect(":memory:")
+import_path = 'your_Directory'
+descrip = 'Control-'   ### Change this to
 
-sqlite_file = '/home/summerintern18/Torgi/GPKG-DATA/TORGI-GNSS-20180826T122015.527Z.gpkg'
-conn = sqlite3.connect(sqlite_file)
-cur = conn.cursor()
-data = cur.execute("SELECT id,svid,constellation,cn0,agc,azimuth_deg,elevation_deg FROM sat_data;")
-with open('satData/output.csv', 'wb') as f:
-    writer = csv.writer(f)
-    writer.writerow(['id', 'svid', 'constellation', 'cn0', 'agc', 'azimuth_deg', 'elevation_deg',"conus", "orig_name", "id_name"])
-    writer.writerows(data)
+print(import_path)
 
-print(cur.fetchall())
+x = 83
 
+for filename in os.listdir(import_path):
+
+    x = x + 1
+
+    if filename.endswith(".gpkg"):
+        con = sqlite3.connect(":memory:")
+
+        sqlite_file = import_path + filename
+        conn = sqlite3.connect(sqlite_file)
+        cur = conn.cursor()
+        data = cur.execute("SELECT id,svid,constellation,cn0,agc,azimuth_deg,elevation_deg FROM Sat_Data;")
+
+        export_csv = descrip + str(x) + '.csv'
+
+        with open(export_csv, 'wb') as csvf:
+            writer = csv.writer(csvf)
+            writer.writerow(
+                ['id', 'svid', 'constellation', 'cn0', 'agc', 'azimuth_deg', 'elevation_deg',"fileName", "CONUS", "fileID" , filename, 0 , descrip + str(x)])
+            writer.writerows(data)
+            conn.close()
 
 
 

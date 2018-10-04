6d32bcdc1a48d5884074ce5cdb797d6a60aceb2f
Jacob Fletcher <jacobfletcher0293@floridapoly.edu>
2018-10-04 10:48:23 -0400

    FIxed Pysqlite
    Adds CONUS, fileneame and , fileId to csv rows

--- analysis/Python/main.py
+++ analysis/Python/main.py
@@ -4,17 +4,17 @@ import os
 import pandas as pd
 
 con = sqlite3.connect(":memory:")
-import_path = 'your_Directory'
-descrip = 'Control-'   ### Change this to
-
+import_path = '/home/summerintern18/Torgi/SatData/Unprocessed/'
+descrip = 'Control-'
 print(import_path)
-
 x = 83
 
+
+def individual_output():
 for filename in os.listdir(import_path):
 
     x = x + 1
-
+    fileId = descrip + str(x)
     if filename.endswith(".gpkg"):
         con = sqlite3.connect(":memory:")
 
@@ -28,10 +28,15 @@ for filename in os.listdir(import_path):
         with open(export_csv, 'wb') as csvf:
             writer = csv.writer(csvf)
             writer.writerow(
-                ['id', 'svid', 'constellation', 'cn0', 'agc', 'azimuth_deg', 'elevation_deg',"fileName", "CONUS", "fileID" , filename, 0 , descrip + str(x)])
+                ['id', 'svid', 'constellation', 'cn0', 'agc', 'azimuth_deg', 'elevation_deg',"fileName", "CONUS", "fileId" ])
             writer.writerows(data)
             conn.close()
-
-
-
-
+    engine ='python'
+    output_df = pd.read_csv(export_csv, export_csv)
+    engine = 'python'
+    output_df = pd.read_csv(export_csv, export_csv)
+    output_df['CONUS'] = '0'
+    output_df['fileID'] = fileId
+    output_df['fileName'] = filename
+    count = output_df.count()
+    output_df.to_csv(export_csv)

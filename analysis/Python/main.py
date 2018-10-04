ada06b72f913c18f1b11b9bef3f52e71ff3915f8
Jacob Fletcher <jacobfletcher0293@floridapoly.edu>
2018-10-04 11:09:38 -0400

    Update main.py

--- analysis/Python/main.py
+++ analysis/Python/main.py
@@ -3,40 +3,47 @@ import csv
 import os
 import pandas as pd
 
-con = sqlite3.connect(":memory:")
-import_path = '/home/summerintern18/Torgi/SatData/Unprocessed/'
-descrip = 'Control-'
-print(import_path)
-x = 83
-
 
 def individual_output():
-for filename in os.listdir(import_path):
+    con = sqlite3.connect(":memory:")
+    import_path = '/home/summerintern18/Torgi/GPKG-DATA/'
+    descrip = 'Control-'
+    print(import_path)
+    x = 83
+    for filename in os.listdir(import_path):
+        try:
+            x = x + 1
+            fileId = descrip + str(x)
+            if filename.endswith(".gpkg"):
+                con = sqlite3.connect(":memory:")
+                print(filename)
+                sqlite_file = import_path + filename
+                conn = sqlite3.connect(sqlite_file)
+                cur = conn.cursor()
+                data = cur.execute("SELECT id,svid,constellation,cn0,agc,azimuth_deg,elevation_deg FROM sat_Data;")
 
-    x = x + 1
-    fileId = descrip + str(x)
-    if filename.endswith(".gpkg"):
-        con = sqlite3.connect(":memory:")
+                export_csv = descrip + str(x) + '.csv'
 
-        sqlite_file = import_path + filename
-        conn = sqlite3.connect(sqlite_file)
-        cur = conn.cursor()
-        data = cur.execute("SELECT id,svid,constellation,cn0,agc,azimuth_deg,elevation_deg FROM Sat_Data;")
+                with open(export_csv, 'wb') as csvf:
+                    writer = csv.writer(csvf)
+                    writer.writerow(
+                        ['id', 'svid', 'constellation', 'cn0', 'agc', 'azimuth_deg', 'elevation_deg',"fileName", "CONUS", "fileId" ])
+                    writer.writerows(data)
+                    conn.close()
 
-        export_csv = descrip + str(x) + '.csv'
+            output_df = pd.read_csv(export_csv, export_csv)
+            output_df['CONUS'] = '0'
+            output_df['fileID'] = fileId
+            output_df['fileName'] = filename
 
-        with open(export_csv, 'wb') as csvf:
-            writer = csv.writer(csvf)
-            writer.writerow(
-                ['id', 'svid', 'constellation', 'cn0', 'agc', 'azimuth_deg', 'elevation_deg',"fileName", "CONUS", "fileId" ])
-            writer.writerows(data)
-            conn.close()
-    engine ='python'
-    output_df = pd.read_csv(export_csv, export_csv)
-    engine = 'python'
-    output_df = pd.read_csv(export_csv, export_csv)
-    output_df['CONUS'] = '0'
-    output_df['fileID'] = fileId
-    output_df['fileName'] = filename
-    count = output_df.count()
-    output_df.to_csv(export_csv)
+            output_df.to_csv(export_csv)
+
+        except sqlite3.Error:
+            print(filename + " is empty")
+
+
+def main():
+    individual_output()
+
+
+main()

d42eeaffdf70f610cf3f30730e61fa04c5128d3c
scottlandis <scottalandis118@gmail.com>
2019-01-31 11:05:40 -0800

    merge with sofwerx

---
+++ torgi/src/viewer/java/org/sofwerx/torgi/ui/FailureActivity.java
@@ -0,0 +1,13 @@
+package org.sofwerx.torgi.ui;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class FailureActivity extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_failure);
+    }
+}
